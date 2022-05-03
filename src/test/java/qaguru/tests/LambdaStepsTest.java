66f3b2c6eee5c5b9a834d73689d0cb60a5910b00
Maksim Golyanitskiy <golyanitskiy@gmail.com>
2022-05-03 20:44:13 +0400

    work done

---
+++ src/test/java/qaguru/tests/LambdaStepsTest.java
@@ -0,0 +1,36 @@
+package qaguru.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class LambdaStepsTest {
+    @BeforeAll
+    static void setUp() {
+        Configuration.headless = true;
+    }
+
+    @AfterAll
+    static void tearDown() {
+        closeWebDriver();
+    }
+
+    @Test
+    public void testIssueSearch() {
+        open("https://github.com");
+
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys("golyanitskiy/qaguru_6_junit");
+        $(".header-search-input").submit();
+
+        $(By.linkText("golyanitskiy/qaguru_6_junit")).click();
+        $(By.partialLinkText("Issues")).click();
+        $(withText("#1")).should(Condition.exist);
+    }
+}
---
+++ src/test/java/qaguru/tests/SelenideOnlyTest.java
@@ -0,0 +1,47 @@
+package qaguru.tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
+
+public class SelenideOnlyTest {
+    @AfterAll
+    static void tearDown() {
+        closeWebDriver();
+    }
+
+    final String REPOSITORY = "golyanitskiy/qaguru_6_junit";
+    final int ISSUE_NUMBER = 1;
+
+    @Test
+    public void testLambdaSteps() {
+
+        step("Open home page", () -> open("https://github.com"));
+        step("Search for a repository" + REPOSITORY, () -> {
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys("schwester34/tests6");
+            $(".header-search-input").submit();
+        });
+
+        step("Open the repository" + REPOSITORY, () -> $(By.linkText("schwester34/tests6")).click());
+        step("Go to Issue tab", () -> $(By.partialLinkText("Issues")).click());
+        step("Checking for Issue tabs" + ISSUE_NUMBER, () -> {
+            $(withText("#1")).should(Condition.exist);
+        });
+    }
+
+    @Test
+    void webSteps() {
+        WebSteps steps = new WebSteps();
+        steps.openMainPage();
+        steps.searchForRepository(REPOSITORY);
+        steps.openRepository(REPOSITORY);
+        steps.openIssueTab();
+        steps.shouldSeeIssueWithNumber(ISSUE_NUMBER);
+    }
+}
